39cec386d02b8737570313064f8a43f1cae63ae7
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-08 14:36:19 +0300

    Added student model

---
+++ CQRS-and-MediatR-Testing/Models/StudentDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace CQRS_and_MediatR_Testing.Models
+{
+    public class StudentDetails
+    {
+        public Guid Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set;}
+        public string StudentAddress { get; set;}
+        public uint StudentAge { get; set; }
+    }
+}
