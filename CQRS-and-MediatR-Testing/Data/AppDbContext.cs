b21c8b56b44c47801d5768a9dc189db904e8b9bc
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-08 14:34:59 +0300

    Added database context

---
+++ CQRS-and-MediatR-Testing/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CQRS_and_MediatR_Testing.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentDetails = CQRS_and_MediatR_Testing.Models.StudentDetails;
+
+namespace CQRS_and_MediatR_Testing.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<StudentDetails> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<StudentDetails>().HasData(new StudentDetails
+            {
+                Id = new Guid("4d8fcf42-6539-4286-a9cf-3cbc8063fa10"),
+                StudentAddress = "testAddress",
+                StudentAge = 20,
+                StudentEmail = "mail@mail.com",
+                StudentName = "Aleksandr"
+            });
+        }
+    }
+}
