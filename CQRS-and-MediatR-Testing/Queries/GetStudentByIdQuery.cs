4d64f5af956d9489776e9d0147127347d357deec
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-15 09:08:31 +0300

    added queries

---
+++ CQRS-and-MediatR-Testing/Queries/GetStudentByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using CQRS_and_MediatR_Testing.Models;
+using MediatR;
+
+namespace CQRS_and_MediatR_Testing.Queries
+{
+    public class GetStudentByIdQuery : IRequest<StudentDetails>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ CQRS-and-MediatR-Testing/Queries/GetStudentListQuery.cs
@@ -0,0 +1,9 @@
+﻿using CQRS_and_MediatR_Testing.Models;
+using MediatR;
+
+namespace CQRS_and_MediatR_Testing.Queries
+{
+    public class GetStudentListQuery : IRequest<List<StudentDetails>>
+    {
+    }
+}
