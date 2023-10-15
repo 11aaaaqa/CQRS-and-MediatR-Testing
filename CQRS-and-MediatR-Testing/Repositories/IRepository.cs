9ab4b86aa1c45559f496356e9ad0eb1e665d895f
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-15 09:04:54 +0300

    added repository interface and its implementation

---
+++ CQRS-and-MediatR-Testing/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using CQRS_and_MediatR_Testing.Models;
+
+namespace CQRS_and_MediatR_Testing.Repositories
+{
+    public interface IRepository<T>
+    {
+        public Task<List<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(Guid id);
+        public Task AddAsync(T entity);
+        public Task UpdateStudentAsync(T entity);
+        public Task DeleteStudentAsync(Guid id);
+    }
+}
---
+++ CQRS-and-MediatR-Testing/Repositories/StudentRepository.cs
@@ -0,0 +1,46 @@
+﻿using CQRS_and_MediatR_Testing.Data;
+using CQRS_and_MediatR_Testing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS_and_MediatR_Testing.Repositories
+{
+    public class StudentRepository : IRepository<StudentDetails>
+    {
+        private readonly AppDbContext context;
+
+        public StudentRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<StudentDetails>> GetAllAsync() => await context.Students.ToListAsync();
+
+        public async Task<StudentDetails> GetByIdAsync(Guid id)
+        {
+            var student = await context.Students.SingleOrDefaultAsync(x => x.Id == id);
+            if (student == null) throw new ArgumentException("Incorrect student ID");
+            return student;
+        } 
+
+        public async Task AddAsync(StudentDetails entity)
+        {
+            if (await context.Students.FirstOrDefaultAsync(x => x.Id == entity.Id) == null)
+            {
+                context.Students.Add(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateStudentAsync(StudentDetails entity)
+        {
+            context.Students.Update(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteStudentAsync(Guid id)
+        {
+            context.Students.Remove(new StudentDetails { Id = id });
+            await context.SaveChangesAsync();
+        }
+    }
+}
