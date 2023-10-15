052d31c553986f1e575b92d3bdc223a2b2f718d1
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-15 09:08:22 +0300

    added student controller

---
+++ CQRS-and-MediatR-Testing/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS_and_MediatR_Testing.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
