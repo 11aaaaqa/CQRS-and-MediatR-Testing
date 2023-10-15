7299c6ead451f18cd79d3661502be827ea642819
aaaaqa <Kvadrocopter@DESKTOP-S5LDS07>
2023-10-15 09:09:10 +0300

    added student actions handler

---
+++ CQRS-and-MediatR-Testing/Handlers/GetStudentByIdHandler.cs
@@ -0,0 +1,19 @@
+﻿using CQRS_and_MediatR_Testing.Models;
+using CQRS_and_MediatR_Testing.Queries;
+using CQRS_and_MediatR_Testing.Repositories;
+using MediatR;
+
+namespace CQRS_and_MediatR_Testing.Handlers
+{
+    public class GetStudentByIdHandler : IRequestHandler<GetStudentByIdQuery,StudentDetails>
+    {
+        private readonly IRepository<StudentDetails> repository;
+
+        public GetStudentByIdHandler(IRepository<StudentDetails> repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<StudentDetails> Handle(GetStudentByIdQuery request, CancellationToken cancellationToken) => await repository.GetByIdAsync(request.Id);
+        
+    }
+}
---
+++ CQRS-and-MediatR-Testing/Handlers/GetStudentListHandler.cs
@@ -0,0 +1,21 @@
+﻿using CQRS_and_MediatR_Testing.Models;
+using CQRS_and_MediatR_Testing.Queries;
+using CQRS_and_MediatR_Testing.Repositories;
+using MediatR;
+
+namespace CQRS_and_MediatR_Testing.Handlers
+{
+    public class GetStudentListHandler : IRequestHandler<GetStudentListQuery,List<StudentDetails>>
+    {
+        private readonly IRepository<StudentDetails> repository;
+
+        public GetStudentListHandler(IRepository<StudentDetails> repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<List<StudentDetails>> Handle(GetStudentListQuery request, CancellationToken cancellationToken)
+        {
+            return await repository.GetAllAsync();
+        }
+    }
+}
